7dfcabb99724416792a68e17a6c1061626364a1c
Mike <mike@heliotrope.digital>
2023-04-29 13:41:39 +0200

    Start addNewTaskHandler

--- front/react/src/components/NewTaskForm/NewTaskForm.js
+++ front/react/src/components/NewTaskForm/NewTaskForm.js
@@ -1,21 +1,32 @@
-import React, { useState } from "react";
+import React, { useCallback, useState } from "react";
 import "./NewTaskForm.css";
 
 function NewTaskForm() {
   const [title, setTitle] = useState("");
   const [description, setDescription] = useState("");
 
+  const addNewTaskHandler = useCallback(() => {
+    const form = document.querySelector(".form");
+    form.addEventListener("submit", (e) => {
+      e.preventDefault();
+
+      console.log(title, description);
+    });
+  }, [description, title]);
+
   return (
     <section className="new-task-section">
       <div className="new-task-container">
         <h1>Create New Task</h1>
-        <form>
+        <form className="form">
           <div className="form-group">
             <input
               type="text"
               className="form-control"
               placeholder="Title"
               required
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
             />
           </div>
           <div className="form-group">
@@ -24,9 +35,15 @@ function NewTaskForm() {
               className="form-control description-input"
               placeholder="Description"
               required
+              value={description}
+              onChange={(e) => setDescription(e.target.value)}
             />
           </div>
-          <button type="submit" className="btn btn-primary add-button">
+          <button
+            type="submit"
+            className="btn btn-primary add-button"
+            onClick={addNewTaskHandler}
+          >
             Create
           </button>
         </form>
